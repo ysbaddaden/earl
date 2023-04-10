7304d31af4b6fd469754143b04c71253a0b659e9
Julien Portalier <julien@portalier.com>
2023-04-10 19:30:34 +0200

    Fix: abstract method warning

--- src/sock_server.cr
+++ src/sock_server.cr
@@ -19,7 +19,7 @@ module Earl
   abstract class SockServer < Supervisor
     # Called in a dedicated `Fiber` when a server receives a connection.
     # Connections are closed when the method returns or raised.
-    abstract def call(client : Socket)
+    abstract def call(socket : Socket)
 
     # Adds a TCP server.
     def add_tcp_listener(host : String, port : Int32, *, backlog = ::Socket::SOMAXCONN) : Nil
