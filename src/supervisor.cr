4590d84a91e8009218af5b21199ab47fc0e68c2e
Julien Portalier <julien@portalier.com>
2023-04-10 19:31:53 +0200

    Small enhancements to Supervisor

--- src/supervisor.cr
+++ src/supervisor.cr
@@ -5,6 +5,14 @@ require "./agent"
 module Earl
   # Supervises other agents.
   #
+  # The list of monitored agents is fixed and all agents must be added to the
+  # supervisor before the supervisor starts.
+  #
+  # Use a `Supervisor` to monitor long lived agents that shall be spawned before
+  # the program starts and expectd expected to stay alive for the whole duration
+  # of the program, or at worst will stop by themselves after some time and
+  # won't ever need to be restarted.
+  #
   # - Spawns agents in their dedicated `Fiber`.
   # - Recycles and restarts crashed agents.
   # - Eventually stops when all agents have stopped.
@@ -20,28 +28,36 @@ module Earl
 
     # Adds an agent to supervise.
     def monitor(agent : Agent) : Nil
-      if starting?
-        @mutex.synchronize { @agents << agent }
-      else
-        raise ArgumentError.new("agents must be monitored before starting the supervisor")
+      raise ArgumentError.new("agents must be monitored before starting the supervisor") unless starting?
+      raise ArgumentError.new("can't monitor running agents") unless agent.starting?
+
+      if message = monitor?(agent)
+        raise ArgumentError.new(message)
+      end
+    end
+
+    protected def monitor?(agent : Agent) : String?
+      @mutex.synchronize do
+        return "can't monitor the same agent twice" if @agents.includes?(agent)
+        @agents << agent
+        nil
       end
     end
 
     # Spawns all agents to supervise in their dedicated `Fiber`. Blocks until
     # all agents have stopped.
-    def call
-      agents = @agents
-      @group.add(agents.size)
+    def call : Nil
+      @agents.each { |agent| spawn_agent(agent) }
+      @group.wait
+    end
 
-      agents.each do |agent|
-        ::spawn do
-          while running? && agent.starting?
-            agent.start(link: self)
-          end
+    protected def spawn_agent(agent : Agent) : Nil
+      ::spawn do
+        while running? && agent.starting?
+          agent.start(link: self)
         end
       end
-
-      @group.wait
+      @group.add(1)
     end
 
     # Recycles and restarts crashed agents. Take note that an agent has stopped.
@@ -49,11 +65,15 @@ module Earl
       if exception
         Logger.error(agent, exception)
         log.error { "#{agent.class.name} crashed (#{exception.class.name})" }
-        return agent.recycle if running?
+
+        if running?
+          agent.recycle
+          return
+        end
       end
 
       @group.done
-      Fiber.yield # TODO: sleep(0.seconds) ?
+      sleep(0.seconds)
     end
 
     # Asks all supervised agents to stop.
