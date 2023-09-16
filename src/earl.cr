247a7e2833861ccce8b496e114e420e600b3438f
Julien Portalier <julien@portalier.com>
2023-09-16 17:21:13 +0200

    Fix: missing require for earl/logger

--- src/earl.cr
+++ src/earl.cr
@@ -5,6 +5,7 @@ require "./mailbox"
 require "./pool"
 require "./registry"
 require "./supervisor"
+require "./logger"
 
 # For a generic introduction see `README.md`. For a formal description see
 # `SPEC.md`.
