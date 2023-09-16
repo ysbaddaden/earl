8a07630423df248fe2f1120124da7e820de9408c
Julien Portalier <julien@portalier.com>
2023-09-16 14:34:14 +0200

    Revert "Add DynamicSupervisor (#18)"
    This reverts commit 6d66924889394d854a9e99c07729fa543f1d9c49.
    
    Like Erlang and unlike Elixir, we should only have a single Supervisor
    class that does both static (a fixed list of agents to monitor) at the
    same time.

--- src/earl.cr
+++ src/earl.cr
@@ -5,7 +5,6 @@ require "./mailbox"
 require "./pool"
 require "./registry"
 require "./supervisor"
-require "./dynamic_supervisor"
 
 # For a generic introduction see `README.md`. For a formal description see
 # `SPEC.md`.
