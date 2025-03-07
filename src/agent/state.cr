9232dfa50190810ea64dd99641a40fe8b8de3f12
Julien Portalier <julien@portalier.com>
2025-03-07 12:58:36 +0100

    Fix: update agent state atomically (MT safety)
    closes #16

--- src/agent/state.cr
+++ src/agent/state.cr
@@ -11,8 +11,8 @@ module Earl
       # `Starting` to be able to transition.
       Running
 
-      # The agent has been asked to stop. Previous state must be
-      # `Running` to be able to transition.
+      # The agent has been asked to stop. Previous state must be `Running` to be
+      # able to transition.
       Stopping
 
       # The agent has stopped. Previous state must be `Stopping` to be able to
@@ -20,7 +20,7 @@ module Earl
       Stopped
 
       # The agent has crashed (i.e. raised an exception). Previous state must be
-      # `Running`, `Stopping` or `Crashed` to be able to transition.
+      # `Running` or `Stopping` to be able to transition.
       Crashed
 
       # The agent has been told to recycle. Previous state must be `Stopped` or
@@ -29,41 +29,44 @@ module Earl
     end
 
     # :nodoc:
+    #
+    # Finite state machine that maintains the `Status` of an `Agent`.
     struct State
-      # Finite state machine that maintains the `Status` of an `Agent`.
-
       def initialize
-        @status = Status::Starting
+        @status = Atomic(Status).new(Status::Starting)
       end
 
       def value : Status
-        @status
-      end
-
-      def can_transition?(new_status : Status) : Bool
-        case @status
-        when .starting?
-          new_status.running?
-        when .running?
-          new_status.stopping? || new_status.crashed?
-        when .stopping?
-          new_status.stopped? || new_status.crashed?
-        when .stopped?, .crashed?
-          new_status.recycling?
-        when .recycling?
-          new_status.starting?
-        else
-          false
-        end
+        @status.get(:relaxed)
       end
 
       def transition(agent : Agent, new_status : Status) : Nil
-        if can_transition?(new_status)
-          agent.log.debug { "transition from=#{@status} to=#{new_status}" }
-          @status = new_status
-        else
-          agent.log.error { "transition error from=#{@status} to=#{new_status}" }
-          raise TransitionError.new("can't transition agent #{agent.class.name} state from #{@status} to #{new_status}")
+        old_status = @status.get(:relaxed)
+
+        loop do
+          if can_transition?(old_status, new_status)
+            agent.log.debug { "transition from=#{old_status} to=#{new_status}" }
+            old_status, success = @status.compare_and_set(old_status, new_status, :acquire_release, :relaxed)
+            break if success
+          else
+            agent.log.error { "transition error from=#{old_status} to=#{new_status}" }
+            raise TransitionError.new("can't transition agent #{agent.class.name} state from #{old_status} to #{new_status}")
+          end
+        end
+      end
+
+      private def can_transition?(old_status : Status, new_status : Status) : Bool
+        case old_status
+        in Status::Starting
+          new_status == Status::Running
+        in Status::Running
+          new_status == Status::Stopping || new_status == Status::Crashed
+        in Status::Stopping
+          new_status == Status::Stopped || new_status == Status::Crashed
+        in Status::Stopped, Status::Crashed
+          new_status == Status::Recycling
+        in Status::Recycling
+          new_status == Status::Starting
         end
       end
     end
