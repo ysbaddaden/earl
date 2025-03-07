2a29bbaefad612ae83bea81ebad6e53ce698a951
Julien Portalier <julien@portalier.com>
2025-03-07 15:04:20 +0100

    Fix: application on-exit behavior
    closes #3

--- src/application.cr
+++ src/application.cr
@@ -3,22 +3,26 @@ require "./supervisor"
 
 module Earl
   # A singleton `Supervisor` accessible as `Earl.application` with additional
-  # features suited for programs:
-  #
-  # - traps the SIGINT and SIGTERM signals to exit cleanly;
-  # - adds an `at_exit` handler to ask supervised agents to stop gracefully.
+  # features to gracefully stop the application on exit.
   #
   # Programs must always start `Earl::Application` because some Earl agents
-  # (e.g. log agents) expect that `Earl.application` will be started. It can be
-  # spawned in the background (and forgotten) or better: be leveraged to monitor
-  # the program's agents and block the main `Fiber` until the program is told to
-  # terminate, which is recommended.
+  # (for example `Log` agents) are only started by `Earl.application`.
+  #
+  # While it can be spawned in the background (and forgotten) we recommend to
+  # use it to supervise your program's main agents and block the main `Fiber`
+  # until the program terminates.
   #
   # TODO: support Windows
   class Application < Supervisor
     @once = Atomic(Bool).new(false)
 
-    # List of POSIX signals to trap. Defaults to `SIGINT` and `SIGTERM`.
+    # Application is a singleton object.
+    protected def initialize
+      super
+    end
+
+    # List of POSIX signals to trap. Defaults to `Signal::INT` and
+    # `Signal::TERM`.
     #
     # WARNING: the list may only be mutated prior to starting the application!
     def signals : Array(Signal)
@@ -28,22 +32,32 @@ module Earl
       ]
     end
 
-    # Traps signals. Adds an `at_exit` handler then delegates to `Supervisor`
-    # which will block until all supervised actors are asked to terminate.
+    # Starts the application by deleting to the supervisor.
+    #
+    # Adds an `at_exit` handler to try and gracefully stop if the application
+    # state is still running.
+    #
+    # Registers signal traps to gracefully stop when the signal is received, and
+    # exit immediately when received while the agent state is no longer running.
     def call : Nil
       return if @once.swap(true, :sequentially_consistent)
 
       signals.each do |signal|
         signal.trap do
-          log.debug { "received SIG#{signal} signal" }
-          sleep(0.seconds)
-          exit
+          if running?
+            log.info { "received SIG#{signal} signal: exiting gracefully (send again to force exit)" }
+            stop
+          else
+            log.warn { "received SIG#{signal} signal twice: exiting" }
+            Fiber.yield
+            exit
+          end
         end
       end
 
       # At exit handlers are run in reverse order, but we need this handler to
-      # run after previously setup handler have been set (e.g.
-      # minitest/autorun), so we prepend it the list.
+      # run after previously setup handler have been set (for example
+      # minitest/autorun in the test suite), so we prepend it the list.
       Crystal::AtExitHandlers.__earl_prepend do
         stop if running?
       end
